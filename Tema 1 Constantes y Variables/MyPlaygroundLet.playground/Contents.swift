3ab046bb5a862d391b3e1ca18b256dbc97e9df5d
Juan Morillo <juanmorillios@gmail.com>
2016-07-17 18:43:03 +0200

    Tema 1 - Let - Var
    El tema 1 esta relacionado con las constantes y variables.

---
+++ Tema 1 Constantes y Variables/MyPlaygroundLet.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//Ejercicio de constantes
+
+//Utilizando (String) como valor principal, para el ejercicio ésta opción no es la correcta, ya que en el momento que la persona cumpla años, tendra que modificar la cadena de string.
+let birthYear: String = "26 de noviembre del año 1976"
+
+let name: String = "Juan Morillo"
+
+print("El señor \(name) nacio el \(birthYear)")
+
+
+//Utilizando Número Entero (Int), considero que esta es la mejor opción
+let birthYear2: Int = 1976
+
+print("El señor \(name) nacio el año \(birthYear2)")
+
---
+++ Tema 1 Constantes y Variables/MyPlaygroundVar.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var str = "Hello, playground"
+
+
+
+// Ejercicio 1 de variables
+
+
+var x = 4
+var y = 2
+var a = (x + y) / 2
+
+a
+
+a + 5
+
+a = 2
+
+a
+
+var age: Int = 39
+//Tambien la variable name se puede declarar asi.
+
+var name1: String = "Juan Morillo" // aqui realizamos ambas cosas, declaramos e instanciamos.
+
+print("La fecha de nacimiento de \(name1), es \(age)")
+
+var name = String() // creamos una variable pero no se crea la instancia de la misma
+
+name = "Juan Morillo"
+print("La fecha de nacimiento de \(name), es \(age)")
+
+
+
+
+// Ejercicio 2, Cambio el valor de una variable en ejecución
+
+var weather = "Fenomenal, hoy hace un dia Soleado 🌞, dan ganas de ir a la 🏖"
+
+weather = "Uhh, protegerse, el estado del tiempo para esta tarde ha pasado a ser  Soleado🌞 a Lluvioso ☔️"
+
+
+
+
+
+
+
+
