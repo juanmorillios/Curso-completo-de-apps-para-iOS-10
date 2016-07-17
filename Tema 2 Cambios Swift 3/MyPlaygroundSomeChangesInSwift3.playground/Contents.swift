aeb7eb7d441f149265da69705a82af9256522f2a
Juan Morillo <juanmorillios@gmail.com>
2016-07-17 19:07:20 +0200

    Change in bucle for
    Change in bucle for

--- Tema 2 Cambios Swift 3/MyPlaygroundSomeChangesInSwift3.playground/Contents.swift
+++ Tema 2 Cambios Swift 3/MyPlaygroundSomeChangesInSwift3.playground/Contents.swift
@@ -17,9 +17,46 @@ age += 1 // Cambio en Swift 3
 
 //Larga vida al ++ y --
 
-age++ // Versión anterior aqui el compilador se queja y propone a solucionar el error  por +=
+//age++ // Versión anterior aqui el compilador se queja y propone a solucionar el error  por +=
 
 
 
+
+// Con swift 3 el mismo bucle seria de la siguiente manera.
+
+for i in 0..<10 {
+
+print("Hey he cambiado")
+
+}
+
+
+// cambios en los bucles
+
+//antes ahora mismo da un fallo.
+
+for var i = 0; i <10; i++ {
+    
+    print("Hey he cambiado")
+    
+    
+}
+
+
+//Siempre que vaya a usar una variable en un for y no la utilizará en el proceso.
+//Reemplazala por la _ ejemplo
+
+/*
+ for i in 0..<10 seria:
+ 
+ for _ in 0..<10 {
+ 
+ print("Hola Mundo")
+ 
+ }
+ 
+ 
+ 
+ */
 
 
