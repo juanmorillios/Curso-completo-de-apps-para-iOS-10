f36265fe425c01ea388d4926ddee01bb8d0cd215
Juan Morillo <juanmorillios@gmail.com>
2016-07-17 18:56:36 +0200

    Change in Swift 3
    age++ not, age += 1 is ok in swift 3

---
+++ Tema 2 Cambios Swift 3/MyPlaygroundSomeChangesInSwift3.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// Hay que mirar y entender bien el concepto ABI Stabilización.
+
+// Primer cambio importante en Swift 3 es la eliminación de i++ y i--
+
+
+//antes
+
+var age = 39
+
+
+age += 1 // Cambio en Swift 3
+
+
+age++ // Versión anterior aqui el compilador se queja y propone a solucionar el error  por +=
+
+
+
+
+
