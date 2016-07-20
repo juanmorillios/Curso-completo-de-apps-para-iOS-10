f1b256aec438e70c79fe4d66d7898a1762684ee4
Juan Morillo <juanmorillios@gmail.com>
2016-07-20 19:16:20 +0200

    Castings Numbers
    Castings Numbers

---
+++ Maths/MyPlaygroundCasting.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let pi = 3.1415926536
+
+let radio : Int = 5
+
+
+//Castings
+
+Int(pi) * radio
+
+let num : Float = 23.5
+
+let num2 = 45
+
+/*Para poder realizar la suma debemos realizar un casting de num2, ya que esta declarado como Int, Siwft si tú no asigna el tipo de dato explicitamente, por defecto le asigna Int (num2)  */
+
+let sum = num + Float(num2)
+
+
+
+let dayMonth : Int = 11
+
+let e = 2.71828
+
+let sum2 = dayMonth + Int(e)
+
+let sum3 = Double(dayMonth) + e
+
+
