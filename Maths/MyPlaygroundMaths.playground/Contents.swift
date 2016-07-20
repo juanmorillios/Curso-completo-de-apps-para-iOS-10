c6a7a0fceb3ea1f8056803a237900b535892098b
Juan Morillo <juanmorillios@gmail.com>
2016-07-20 17:53:44 +0200

    Add Maths
    * / + _  %

---
+++ Maths/MyPlaygroundMaths.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Variables
+var num: Int = 5
+var num2 : Int = 5
+
+//Suma
+var sum = num + num2
+
+
+//Resta
+var resta = num - num2
+
+// 1ro. Multiplicación
+var mult = num * num2
+
+// 2do. División
+var division = num / num2
+
+//3ro. Suma
+var sum2 = num + num2
+
+//4to.  Resto
+var resto = num % num2
+
+// Orden de operacíon
+// Multiplicación
+var mult1 = num * num2
+
+// División
+var division1 = num / num2
+
+//Ejemplo orden operación
+(num * num2) + 4 - 8
+
+
+
