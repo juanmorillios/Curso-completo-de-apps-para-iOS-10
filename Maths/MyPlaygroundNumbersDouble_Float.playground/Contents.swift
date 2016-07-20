f39f1db8098f2b0ebb0edfaa2e7f9e6572ba6743
Juan Morillo <juanmorillios@gmail.com>
2016-07-20 18:58:35 +0200

    Add Numbers Float - Double
    Add Numbers Float - Double

---
+++ Maths/MyPlaygroundNumbersDouble_Float.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//Declaración de constantes explicitas.
+
+let name : String = "Maria Garcia"
+
+let age : Int = 39
+
+let weight : Double = 79.85
+
+
+print("\(name) tienes \(age) y pesa \(weight) kilos")
+
+
+//Valor número e conocido como número de Euler o constante de Napier
+
+let e = 2.71828
+
+/*
+El número áureo es el valor numérico de la proporción que guardan entre sí dos segmentos de recta a y b (a más largo que b), que cumplen la siguiente relación:
+
+La longitud total, suma de los dos segmentos a y b, es al segmento mayor a, lo que este segmento a es al menor b.
+
+
+*/
