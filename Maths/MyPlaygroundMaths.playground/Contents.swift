ed26555c84bed6cd86e5d50110d877a967040f8c
Juan Morillo <juanmorillios@gmail.com>
2016-07-21 19:06:06 +0200

    Math Advance
    Math Advance - functions pow-Sqrt-ceil - floor

--- Maths/MyPlaygroundMaths.playground/Contents.swift
+++ Maths/MyPlaygroundMaths.playground/Contents.swift
@@ -36,4 +36,72 @@ var division1 = num / num2
 (num * num2) + 4 - 8
 
 
+//Matematicas avanzadas - funciones framework foundations
+
+
+var pi = 3.14159265
+
+Int(pi)
+
+//Redondeo a la baja, es igual como el Int.
+floor(pi)
+
+//Redondeo a la alza
+ceil(pi)
+
+
+//Calcular raices cuadradas actuan sobre numeros decimales
+
+sqrt(4.0)
+
+//Pontencia pow
+
+
+pow(2, 4)
+
+
+//Dinero en cartera
+var wallet = 50
+
+//Compra un desayuno
+wallet -= 7
+
+//Compra materiales escolares
+wallet -= 20
+
+//Compra comida
+wallet -= 15
+
+//compra del ticket bus
+
+wallet -= 3
+
+//VALOR QUE ME QUEDA
+
+wallet
+
+
+
+var costeGymMes = 19.68
+
+pow(19, 2)
+
+sqrt(19.68)
+
+floor(costeGymMes)
+
+ceil(costeGymMes)
+
+costeGymMes += 5
+
+costeGymMes -= 5
+
+
+
+
+
+
+
+
+
 
