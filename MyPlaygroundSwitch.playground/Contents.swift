875e5beb0fa2e26537532302ab21d1f75d9fb4d4
Juan Morillo <juanmorillios@gmail.com>
2016-07-26 23:08:50 +0200

    App Weather
    App Weather

--- MyPlaygroundSwitch.playground/Contents.swift
+++ MyPlaygroundSwitch.playground/Contents.swift
@@ -60,7 +60,7 @@ default:
  */
 
 
-var dayOfTheWeek : String = "Vierndes"
+var dayOfTheWeek : String = "Viernes"
 
 switch dayOfTheWeek {
 
@@ -70,11 +70,11 @@ case "Lunes":
     
 case "Martes":
     
-    print("Faltan 3 dias para el viernes")
+    print("Faltan 3 días para el viernes")
 
 case "Miércoles":
     
-    print("Faltan 2 dias para el viernes")
+    print("Faltan 2 días para el viernes")
     
 case "Jueves":
     print("Faltan 1 día para el viernes")
@@ -85,6 +85,66 @@ case "Viernes":
 default:
     print("Escribe un dia de la semana y te confirmo cúantos días faltan para llegar al viernes")
 }
+
+
+var character = "a"
+
+switch character {
+case "a", "e", "i", "o", "u":
+    print("Es vocal")
+default:
+    print("No es vocal")
+}
+
+
+/*
+ 
+ Crea un switch que tome un entero por parámetro. Si el número es 2, 3, 5 o 7 imprime "Número primo". Asegúrate de no poner 4 casos!!
+  */
+
+let numerosPrimos = 3
+
+switch numerosPrimos {
+case 2, 3, 5, 7:
+    print("\(numerosPrimos) es un número primo")
+default:
+    print("\(numerosPrimos) no es un número primo")
+}
+
+
+
+/*
+ Crea un switch para las horas del día en formato 24 horas. Utiliza varios casos para poder decir si estamos en el amanecer, mañana, mediodía, tarde, noche.
+ */
+
+let horasDelDia : Double = 06.00
+
+switch horasDelDia {
+    
+case 01.00...05.00:
+    
+    print("Madrugada")
+    
+case 05.01...06.00:
+    
+    print("Que lindo amanecer")
+case 06.01...13.00:
+    
+    print("Ya es mediodía")
+
+case 13.01...20.00:
+   
+    print("Buenas tardes")
+
+case 20.01...24.00:
+    
+    print("Buenas noches")
+    
+default:
+    
+    print("No corresponde a una hora del día")
+}
+
 
 
 
