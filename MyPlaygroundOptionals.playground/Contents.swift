81ba377adb9dfd94eb26d28ff024294308d69c5e
Juan Morillo <juanmorillios@gmail.com>
2016-08-01 16:44:49 +0200

    Valor Optionals
    Valor Optionals

---
+++ MyPlaygroundOptionals.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Una variable nula, es una variable que no tiene valor, ejemplo:
+
+var num : String
+
+num = "Hola"
+
+print(num)
+
+//num = nil // esto no se puede hacer, porque estamos intentanto eliminar un objeto, se puede utilizar para eliminar variables una vez dejan de ser útiles
+
+
+var dict  = [3:"Tres"]
+
+dict[3]
+dict[2] // aqui el valor es nil, porque el elemento del diccionario número 2 no esta incorporado en el diccionario.
+
+
+// verificar los valores nulos = nil
+
+
+
+
+
